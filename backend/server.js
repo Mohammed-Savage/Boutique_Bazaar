aedf43c8c2b2c8c5e7f0c0edcc7bd3fe1ffdcbf9
Mohammed Omar <omartechmohammed@gmail.com>
2025-05-19 17:53:52 -0400

    Created and tested working DELETE functionality with POSTMAN

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,17 @@ app.post("/api/products", async (req, res) => {
     }
 });
 
+app.delete("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted successfully" });
+    } catch (error) {
+        console.error("Error in Delete product:", error.message);
+        res.status(500).json({ success: false, message: "Prodcut not found in inventory" });
+    }
+});
+
 app.listen(5000, () => {
     connectDB();
     console.log("Server is up and running at http://localhost:5000");
